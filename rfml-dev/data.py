c3528b448212257e21e7c8be68c15ca45d92a8c2
Lucas Tindall <ltindall@ucsd.edu>
2024-02-09 22:25:14 -0800

    Add SigMF annotations from labelme

--- rfml-dev/data.py
+++ rfml-dev/data.py
@@ -21,8 +21,17 @@ SIGMF_META_DEFAULT = {
         }
     ],
     "annotations": [],  # https://github.com/sigmf/SigMF/blob/sigmf-v1.x/sigmf-spec.md#annotation-segment-objects
-    "spectrograms": [],
+    "spectrograms": {},
     # spectrogram should have (img_filename, sample_start, sample_count, nfft, augmentations)
+}
+
+SIGMF_ANNOTATION_DEFAULT = {
+    "core:sample_start": None,
+    "core:sample_count": None,
+    "core:freq_lower_edge": None,
+    "core:freq_upper_edge": None,
+    "core:label": None,
+    "core:comment": None,
 }
 
 
@@ -67,7 +76,6 @@ class Data:
         self.metadata = json.load(open(self.sigmf_meta_filename))
 
     def write_sigmf_meta(self, sigmf_meta):
-
         with open(self.sigmf_meta_filename, "w") as outfile:
             print(f"Saving {self.sigmf_meta_filename}\n")
             outfile.write(json.dumps(sigmf_meta, indent=4))
@@ -89,20 +97,56 @@ class Data:
 
         self.write_sigmf_meta(sigmf_meta)
 
-    def labelme_to_sigmf(self, labelme_json):
-        
+    def labelme_to_sigmf(self, labelme_json, labelme_dir):
+
+        spectrogram_metadata = self.metadata["spectrograms"][str(Path(labelme_dir, labelme_json["imagePath"]))]
         
         sample_rate = self.metadata["global"]["core:sample_rate"]
-        unix_timestamp = datetime.datetime.strptime(self.metadata["captures"][0]["core:datetime"], "%Y-%m-%dT%H:%M:%S.%f%z").timestamp()
-        sample_start_time = unix_timestamp + (self.metadata["sample_start_idx"] / sample_rate)
-        sample_end_time = unix_timestamp + (metadata["sample_end_idx"] / sample_rate)
-        freq_dim = metadata["nfft"]
-        time_dim = mini_batch_size / freq_dim
-        time_space = np.linspace(start=sample_end_time, stop=sample_start_time, num=int(time_dim))
-        freq_center = metadata["sample_file"]["freq_center"]
-        min_freq = freq_center - (sample_rate/2)
-        max_freq = freq_center + (sample_rate/2)
+        unix_timestamp = datetime.strptime(
+            self.metadata["captures"][0]["core:datetime"], "%Y-%m-%dT%H:%M:%S.%f%z"
+        ).timestamp()
+        sample_start_time = unix_timestamp + (
+            spectrogram_metadata["sample_start"] / sample_rate
+        )
+        sample_end_time = unix_timestamp + ((spectrogram_metadata["sample_start"]+spectrogram_metadata["sample_count"])/ sample_rate)
+        freq_dim = spectrogram_metadata["nfft"]
+        time_dim = spectrogram_metadata["sample_count"] / freq_dim
+        time_space = np.linspace(
+            start=sample_end_time, stop=sample_start_time, num=int(time_dim)
+        )
+        sample_space = np.linspace(
+            start=spectrogram_metadata["sample_start"], stop=(spectrogram_metadata["sample_start"]+spectrogram_metadata["sample_count"]), num=int(time_dim)+1
+        )
+
+        freq_center = self.metadata["captures"][0]["core:frequency"]
+        min_freq = freq_center - (sample_rate / 2)
+        max_freq = freq_center + (sample_rate / 2)
         freq_space = np.linspace(start=min_freq, stop=max_freq, num=int(freq_dim))
+
+        new_annotations = 0
+        for labelme_annotation in labelme_json["shapes"]: 
+            sigmf_annotation = SIGMF_ANNOTATION_DEFAULT.copy()
+ 
+            sigmf_annotation["core:sample_start"] = int(sample_space[int(labelme_annotation["points"][0][1])])
+            sigmf_annotation["core:sample_count"] = int(sample_space[int(labelme_annotation["points"][1][1])+1]) - sigmf_annotation["core:sample_start"]
+            sigmf_annotation["core:freq_lower_edge"] = freq_space[int(labelme_annotation["points"][0][0])] # min_freq
+            sigmf_annotation["core:freq_upper_edge"] = freq_space[int(labelme_annotation["points"][1][0])] # max_freq
+            sigmf_annotation["core:label"] = labelme_annotation["label"]
+            sigmf_annotation["core:comment"] = "labelme"
+
+
+            found_in_sigmf = False
+            for annot in self.metadata["annotations"]:
+                if sigmf_annotation.items() <= annot.items():
+                    found_in_sigmf = True
+            
+            if not found_in_sigmf:
+                new_annotations += 1
+                self.metadata["annotations"].append(sigmf_annotation)
+        
+        if new_annotations:
+            self.write_sigmf_meta(self.metadata)
+
 
     def yolo_to_sigmf(self):
         pass
@@ -133,12 +177,13 @@ def images_to_sigmf(directory):
     # if no metadata saved then must be parsed from images or generated in some way
     metadata_dir = Path(directory, "metadata")
     metadata_files = os.listdir(metadata_dir)
-    
+
     for image_filename in image_files:
-            
         metadata_filename = f"{os.path.splitext(image_filename)[0]}.json"
         if metadata_filename not in metadata_files:
-            raise ValueError(f"Could not find matching metadata file {metadata_filename} for {image_filename}")
+            raise ValueError(
+                f"Could not find matching metadata file {metadata_filename} for {image_filename}"
+            )
 
         metadata = json.load(open(Path(metadata_dir, metadata_filename)))
 
@@ -152,7 +197,7 @@ def images_to_sigmf(directory):
         data_object = Data(metadata["sample_file"]["filename"])
 
         spectrogram_metadata = {
-            "filename": metadata["img_file"],
+            # "filename": metadata["img_file"],
             "sample_start": metadata["sample_start_idx"],
             "sample_count": metadata["mini_batch_size"],
             "nfft": metadata["nfft"],
@@ -162,15 +207,22 @@ def images_to_sigmf(directory):
         }
 
         # Checks if spectrogram_metadata is a subset of any spectrogram in SigMF
-        found_in_sigmf = False
-        for sigmf_spectrogram_metadata in data_object.metadata["spectrograms"]:
-            if spectrogram_metadata.items() <= sigmf_spectrogram_metadata.items():
-                found_in_sigmf = True
-                break
 
-        if not found_in_sigmf:
-            data_object.metadata["spectrograms"].append(spectrogram_metadata)
+        if (metadata["img_file"] not in data_object.metadata["spectrograms"]) or not (
+            spectrogram_metadata.items()
+            <= data_object.metadata["spectrograms"][metadata["img_file"]].items()
+        ):
+            data_object.metadata["spectrograms"][metadata["img_file"]] = spectrogram_metadata
             data_object.write_sigmf_meta(data_object.metadata)
+
+        # for sigmf_spectrogram_metadata in data_object.metadata["spectrograms"]:
+        #     if spectrogram_metadata.items() <= sigmf_spectrogram_metadata.items():
+        #         found_in_sigmf = True
+        #         break
+
+        # if not found_in_sigmf:
+        #     data_object.metadata["spectrograms"].append(spectrogram_metadata)
+        #     data_object.write_sigmf_meta(data_object.metadata)
 
 
 def labels_to_sigmf(directory):
@@ -197,11 +249,12 @@ def labels_to_sigmf(directory):
     metadata_files = os.listdir(metadata_dir)
 
     for label_filename in label_files:
-
         # get metadata
         metadata_filename = f"{os.path.splitext(label_filename)[0]}.json"
         if metadata_filename not in metadata_files:
-            raise ValueError(f"Could not find matching metadata file {metadata_filename} for {label_filename}")
+            raise ValueError(
+                f"Could not find matching metadata file {metadata_filename} for {label_filename}"
+            )
         metadata = json.load(open(Path(metadata_dir, metadata_filename)))
 
         # create Data object
@@ -209,7 +262,7 @@ def labels_to_sigmf(directory):
 
         # format spectrogram metadata
         spectrogram_metadata = {
-            "filename": metadata["img_file"],
+            #"filename": metadata["img_file"],
             "sample_start": metadata["sample_start_idx"],
             "sample_count": metadata["mini_batch_size"],
             "nfft": metadata["nfft"],
@@ -220,51 +273,41 @@ def labels_to_sigmf(directory):
 
         label_metadata = json.load(open(Path(label_dir, label_filename)))
 
-        sigmf_annotation_metadata = {
-            "core:sample_start":"",
-            "core:sample_count":"",
-            "core:freq_lower_edge":"",
-            "core:freq_upper_edge":"",
-            "core:label":"",
-            "core:comment":"",
+        data_object.labelme_to_sigmf(label_metadata, label_dir)
 
+        sigmf_annotation_metadata = {
+            "core:sample_start": "",
+            "core:sample_count": "",
+            "core:freq_lower_edge": "",
+            "core:freq_upper_edge": "",
+            "core:label": "",
+            "core:comment": "",
         }
         # Checks if spectrogram_metadata is a subset of any spectrogram in SigMF
-        found_in_sigmf = False
-        for sigmf_spectrogram_metadata in data_object.metadata["spectrograms"]:
-            if spectrogram_metadata.items() <= sigmf_spectrogram_metadata.items():
-                
-                found_in_sigmf = True
-
-                if (
-                    "labels" in sigmf_spectrogram_metadata 
-                    and "labelme" in sigmf_spectrogram_metadata["labels"] 
-                    and sigmf_spectrogram_metadata["labels"]["labelme"] == label_metadata
-                ):
-                    break
-
-                if "labels" not in sigmf_spectrogram_metadata:
-                    sigmf_spectrogram_metadata["labels"] = {}
-                
-                sigmf_spectrogram_metadata["labels"]["labelme"] = label_metadata
-                
-                
+        if (metadata["img_file"] in data_object.metadata["spectrograms"]) and (
+            spectrogram_metadata.items()
+            <= data_object.metadata["spectrograms"][metadata["img_file"]].items()
+        ):
+            if "labels" not in data_object.metadata["spectrograms"][metadata["img_file"]]: 
+                data_object.metadata["spectrograms"][metadata["img_file"]]["labels"] = {}
+            
+            if (
+                "labelme" not in data_object.metadata["spectrograms"][metadata["img_file"]]["labels"]
+                or data_object.metadata["spectrograms"][metadata["img_file"]]["labels"]["labelme"] != label_metadata
+            ):
+                data_object.metadata["spectrograms"][metadata["img_file"]]["labels"].update(
+                    {
+                        "labelme": label_metadata
+                    }
+                )
                 data_object.write_sigmf_meta(data_object.metadata)
-                break
-                
-        
-        if not found_in_sigmf:
-            spectrogram_metadata["labels"] = {
-                "labelme": label_metadata
-            }
-            data_object.metadata["spectrograms"].append(spectrogram_metadata)
+
+        else:
+            spectrogram_metadata["labels"] = {"labelme": label_metadata}
+            data_object.metadata["spectrograms"][metadata["img_file"]] = spectrogram_metadata
             data_object.write_sigmf_meta(data_object.metadata)
+           
 
-        
-        
-
-        
-        
 
 if __name__ == "__main__":
     # /Users/ltindall/data/gamutrf/gamutrf-arl/01_30_23/mini2/snr_noise_floor/
